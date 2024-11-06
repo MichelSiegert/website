8c2175282ca9c4f45ff2bf1f1e17b356720216a5
Michel Siegert <michel@siegert.online>
2024-11-06 11:30:51 +0100

    adding an alt tag for backup.

--- src/mainPage/language/lang.js
+++ src/mainPage/language/lang.js
@@ -14,13 +14,13 @@ function Lang(){
     return (
         <div className="languagesSection">
             <div className="Iconbar">
-                <img className={index ==0?"icon":"unselectedIcon"} onClick={()=>setIndex(0)} src={ts}></img>
-                <img className={index ==1?"icon":"unselectedIcon"} onClick={()=>setIndex(1)} src={js}></img>
-                <img className={index ==2?"icon":"unselectedIcon"} onClick={()=>setIndex(2)} src={java}></img>
-                <img className={index ==3?"icon":"unselectedIcon"} onClick={()=>setIndex(3)} src={kotlin}></img>
-                <img className={index ==4?"icon":"unselectedIcon"} onClick={()=>setIndex(4)} src={python}></img>
-                <img className={index ==5?"icon":"unselectedIcon"} onClick={()=>setIndex(5)} src={dart}></img>
-                <img className={index ==6?"icon":"unselectedIcon"} onClick={()=>setIndex(6)} src={go}></img>
+                <img className={index === 0?"icon":"unselectedIcon"} onClick={()=>setIndex(0)} src={ts} alt="ts"></img>
+                <img className={index === 1?"icon":"unselectedIcon"} onClick={()=>setIndex(1)} src={js} alt="js"></img>
+                <img className={index === 2?"icon":"unselectedIcon"} onClick={()=>setIndex(2)} src={java} alt="java"></img>
+                <img className={index === 3?"icon":"unselectedIcon"} onClick={()=>setIndex(3)} src={kotlin} alt="kotlin"></img>
+                <img className={index === 4?"icon":"unselectedIcon"} onClick={()=>setIndex(4)} src={python} alt="python"></img>
+                <img className={index === 5?"icon":"unselectedIcon"} onClick={()=>setIndex(5)} src={dart} alt="dart"></img>
+                <img className={index === 6?"icon":"unselectedIcon"} onClick={()=>setIndex(6)} src={go} alt="go"></img>
             </div>
             <div className="mainBody">
             Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum
