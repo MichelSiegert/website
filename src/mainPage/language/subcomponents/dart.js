4e1cffda523643a1d004e23265884d0f7c294e26
Michel Siegert <michel@siegert.online>
2024-11-06 19:30:46 +0100

    adding new Accent Color. Adding different texts for different Languages.

--- src/mainPage/language/subcomponents/dart.js
+++ src/mainPage/language/subcomponents/dart.js
@@ -2,8 +2,21 @@
 function Dart(){
     
     return(<div>
-        Dart
-    </div>)
+Dart, combined with the Flutter framework, is a powerful tool for building high-performance, cross-platform mobile applications. With a solid background in Dart, I specialize in developing seamless, responsive apps for both iOS and Android, using Flutter’s rich set of widgets and tools to create intuitive user interfaces and smooth user experiences.
+
+In my experience with Dart and Flutter, I have worked on various projects, employing these essential tools and libraries:
+
+<div>
+    <ul>
+        <li><strong>Framework:</strong> Flutter</li>
+        <li><strong>State Management:</strong>Redux</li>
+        <li><strong>Navigation:</strong> Flutter Navigator</li>
+        <li><strong>UI Design:</strong> Custom Widgets, Material Design, Cupertino Widgets</li>
+        <li><strong>Database:</strong> Hive, Firebase, SQLite</li>
+        <li><strong>Testing:</strong> Flutter Test, Mockito, Integration Testing</li>
+    </ul>
+</div>
+Using Dart’s concise syntax and Flutter's powerful capabilities, I create high-quality mobile applications that provide fast, responsive experiences, whether users are interacting with the app on Android or iOS devices. From developing custom UIs to implementing robust state management solutions, my focus is on delivering apps that are not only functional but also visually appealing and easy to navigate.    </div>)
 }
 
 export default Dart
--- src/mainPage/language/subcomponents/java.js
+++ src/mainPage/language/subcomponents/java.js
@@ -1,8 +1,17 @@
 
 function Java(){
     
-    return(<div>
-        Java
+    return(
+    <div>
+        Java is a cornerstone of enterprise software development, powering scalable, secure, and high-performance applications across industries. With extensive experience in Java, I specialize in creating robust backend systems and well-structured applications that deliver reliability and efficiency.
+        I am skilled in working with a range of popular Java frameworks, tools, and libraries, including:
+        <div>
+            <ul>
+                <li><strong>Backend Development:</strong> Spring Boot, Spring MVC, Hibernate</li>
+               <li><strong>Testing & Automation:</strong> JUnit, Mockito, Cucumber</li>
+            </ul>
+        </div>
+        Through leveraging these frameworks, I build Java applications that are both modular and maintainable, adhering to industry best practices. Whether developing RESTful APIs, working with relational databases, I emphasize performance and scalability at every step. By choosing Java, I deliver software that meets the rigorous demands of today’s enterprise environments.
     </div>)
 }
 
