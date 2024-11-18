2b5b47ab52a4c52052747691118280decd479699
Michel Siegert <michel@siegert.online>
2024-11-18 20:57:46 +0100

    fixing a bug where The path was not consistent. Changing blog title.

--- src/Blog/Blog.js
+++ src/Blog/Blog.js
@@ -12,7 +12,7 @@ function Blog(){
         >
             <div className="entriesText">Entries</div>
             <Link to="/blog/finance1" className="blogCard">
-                <div className="headlineBlog">Getting back into finance via python ML. Part 1 </div>
+                <div className="headlineBlog">Exploring Trading Bot Logic: From Basics to Transformers Part 1 </div>
                 <div className="blogCardText">I’m planning to dive into building the logic for a trading bot using TensorFlow, trying it out as a mixed model. I’ll start with some simple trading logic, move on to using an LSTM, and then see if I can implement a Transformer. The idea is to learn more about these models and how they work in practice.</div>
             </Link>
             <Link to="/blog/helloworld1" className="blogCard">
--- src/Blog/blogPosts/blogList.js
+++ src/Blog/blogPosts/blogList.js
@@ -1,7 +1,7 @@
 import TodoPost from "./BlogPost1/BlogPost1";
 import FinanceBlog from "./pythonFinanaceIdeas/financeChapter1";
 const blogEntries = [
-    { path: '/blog/helloworld1', element: <TodoPost /> },
-    { path: '/blog/finance1', element: <FinanceBlog /> },
+    { path: '/Blog/helloworld1', element: <TodoPost /> },
+    { path: '/Blog/finance1', element: <FinanceBlog /> },
 ]
 export default blogEntries
