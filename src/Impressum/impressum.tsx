c4825ec1a6c88c6f627c26361b7ca69693626c78
Michel Siegert <michel@siegert.online>
2026-01-01 17:16:56 +0100

    adding translation for the footer

--- src/Impressum/impressum.tsx
+++ src/Impressum/impressum.tsx
@@ -1,41 +1,51 @@
 import { NavLink } from 'react-router';
 import './impressum.css';
+import { useTranslation } from 'react-i18next';
 
 function Impressum() {
   const handleClick = () => {
     window.scrollTo(0, 0);
   };
+  const { t } = useTranslation();
 
   return (
     <div id="impressum" className="pl-10 max-w-full pt-3">
       <div className="justify-items-center grid">
-        <div className="text-3xl pb-5">Impressum </div>
+        <div className="text-3xl pb-5">
+          {t('imprint.title')}
+        </div>
+
         <div className="impressumItems w-full justify-evenly text-lg">
           <div className="impressumColumn">
-            <div>Website Owner / Operator:</div>
-            <div>Name: Michel Siegert</div>
-            <div>Contact Information</div>
-          </div>
-          <div className="impressumColumn">
-            <div>Address:</div>
-            <div>Waisenhofstraße 27</div>
-            <div>24103 Kiel </div>
-            <div> Germany</div>
+            <div>{t('imprint.ownerTitle')}</div>
+            <div>{t('imprint.name')}</div>
+            <div>{t('imprint.contact')}</div>
           </div>
 
           <div className="impressumColumn">
-            <div> Email: Michel@Siegert.online</div>
-            <div>Phone: +49 177 87 207 96</div>
+            <div>{t('imprint.addressTitle')}</div>
+            <div>{t('imprint.street')}</div>
+            <div>{t('imprint.city')}</div>
+            <div>{t('imprint.country')}</div>
           </div>
-          <div className="max-w-52 impressumColumn"> Content Responsible in accordance with § 18 (2) MStV: Michel Siegert, Waisenhofstraße 27, 24103 Kiel.</div>
+
+          <div className="impressumColumn">
+            <div>{t('imprint.email')}</div>
+            <div>{t('imprint.phone')}</div>
+          </div>
+
+          <div className="max-w-52 impressumColumn">
+            {t('imprint.responsible')}
+          </div>
         </div>
       </div>
+
       <NavLink
         to="/privacy"
         className="text-sm"
-        onClick={() => handleClick()}
+        onClick={handleClick}
       >
-        Privacy Policy
+        {t('imprint.privacy')}
       </NavLink>
     </div>
   );
